c33b80ad2bae8682d2d5ceeabb3dbcd79a88fa4d
Armando Ribamar <armando.ribamar@gmail.com>
2025-07-16 15:36:05 -0300

    atualização do post/page.tsx

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,3 +1,9 @@
+import clsx from 'clsx';
+import { notFound } from 'next/navigation';
+import { PostCoverImage } from '../../../components/PostCoverImage';
+import { PostSummary } from '../../../components/PostSummary';
+import { findPostBySlugCached } from '../../../lib/post/queries';
+
 type PostSlugPageProps = {
   params: Promise<{
     slug: string;
@@ -7,9 +13,37 @@ type PostSlugPageProps = {
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
 
+  let post;
+  try {
+    post = await findPostBySlugCached(slug);
+  } catch {
+    post = undefined;
+  }
+  if (!post) notFound();
+
+  const postLink = `/post/${post.slug}`;
+
   return (
-    <div>
-      <h1 className='text-7xl font-extrabold py-8'>Rota dinâmica: {slug}</h1>
-    </div>
+    <section className={clsx('grid grid-cols-1 gap-8 mb-16 group')}>
+      <PostCoverImage
+        LinkProps={{
+          href: postLink,
+        }}
+        imageProps={{
+          width: 1200,
+          height: 720,
+          src: post.coverImageUrl,
+          alt: post.title,
+        }}
+      />
+      <PostSummary
+        postLink={postLink}
+        postHeading='h1'
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
+        key={'post.id'}
+      />
+    </section>
   );
 }
