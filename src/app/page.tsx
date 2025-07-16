de26fe1b6bc9f828626e9b12ce2b3c1a4eee9e53
Armando Ribamar <armando.ribamar@gmail.com>
2025-07-16 13:19:44 -0300

    estruturando o layout, metadados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from '@/components/Container';
-import { Header } from '@/components/Header';
 import { PostList } from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
@@ -7,8 +5,7 @@ import { PostFeatured } from '../components/PostFeatured';
 
 export default async function Home() {
   return (
-    <Container>
-      <Header />
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -16,9 +13,6 @@ export default async function Home() {
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
-      <footer>
-        <h1 className='text-6xl font-bold text-center py-1'>FOOTER</h1>
-      </footer>
-    </Container>
+    </>
   );
 }
