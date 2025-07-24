72aac18df546a17367e7a94dcb96381d2e3c3a5a
Armando Ribamar <armando.ribamar@gmail.com>
2025-07-24 14:39:06 -0300

    metodos do DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -6,7 +6,11 @@ import { PostRepository } from './post-repository';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAll(): Promise<PostModel[]> {
-    throw new Error('Method not implemented.');
+    const posts = drizzleDb.query.posts.findMany({
+      orderBy: desc(postsTable.createdAt),
+    });
+
+    return posts;
   }
   async findAllPublic(): Promise<PostModel[]> {
     const posts = drizzleDb.query.posts.findMany({
@@ -17,15 +21,32 @@ export class DrizzlePostRepository implements PostRepository {
     return posts;
   }
   async findById(id: string): Promise<PostModel> {
-    throw new Error('Method not implemented.');
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    // Se o post não for encontrado, lance um erro.
+    if (!post) throw new Error('Post não encontrado');
+
+    // Se o código continuar, o TypeScript sabe que 'post' é do tipo PostModel.
+    return post;
   }
   async findBySlugPublic(slug: string): Promise<PostModel> {
-    throw new Error('Method not implemented.');
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { and, eq }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    // Se o post não for encontrado, lance um erro.
+    if (!post) throw new Error('Post não encontrado');
+
+    // Se o código continuar, o TypeScript sabe que 'post' é do tipo PostModel.
+    return post;
   }
 }
 
-(async () => {
+/* (async () => {
   const repo = new DrizzlePostRepository();
   const posts = await repo.findAllPublic();
   posts.forEach(post => console.log(post.slug, post.published));
-})();
+})(); */
